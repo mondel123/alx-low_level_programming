cf93cdf3a04914d4f9c81de0718f077c75f99d5e
mondel123 <adamsmonday1@gmail.com>
2022-09-09 15:27:12 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
  * Return: 0 if success.
  */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }

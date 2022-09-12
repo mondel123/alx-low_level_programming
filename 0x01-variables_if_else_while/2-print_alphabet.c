c381341ecab9ff79d321882f59ff794c6a6beec3
mondel123 <adamsmonday1@gmail.com>
2022-09-12 14:09:07 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (seccess/correct)
+ * Return: Always 0 (success/correct)
  */
 int main(void)
-
 {
-char alphabet;
-
-for (alphabet = 'a'; alphabet <= 'z'; alphabet**)
-{
-putchar(alphabet);
-}
-
-putchar('\n');
-
-return (0);
-
+    char alphabet;
+    for (alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+        putchar(alphabet);
+    }
+    putchar('\n');
+    return (0);
 }

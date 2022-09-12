e511dfd32b8a331cbe486e92ef51a8d3f2da2ccb
mondel123 <adamsmonday1@gmail.com>
2022-09-12 13:35:23 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,24 +5,23 @@
  * Return: Always 0 (seccess/correct)
  */
 int main(void)
-
 {
 char lower_case
 char upper_case
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
-putchar(lower_case);
+	putchar(lower_case);
 }
 
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
-putchar(upper_case);
+	putchar(upper_case);
 }
 
 
-putchar('\n');
+// putchar('\n');
 
-return (0);
+	return 0;
 
 }

1e58c8d1a626cace562269cdbf8ce132e28b5060
mondel123 <adamsmonday1@gmail.com>
2022-09-15 06:52:27 -0700

    multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,22 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i)
-{
-    if (i == 0)
-    {
-        printf("%d is zero", i);
-    }
-    
-    if (i < 0)
-    {
-        printf("%d is negative", i);
-    }
-    
-    if (i > 0)
-    {
-        printf("%d is positive", i);
-    }
-}
+void positive_or_negative(int i);
 
 #endif /* MAIN_H */

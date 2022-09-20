1247a02f21eeeaff1714d191f1d3a1add6992cfb
mondel123 <adamsmonday1@gmail.com>
2022-09-20 14:07:35 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*
+*
+* Description: print
+*
+*
+*
+*/
+int main ()
+{
+int n1 = 0, n2, n3;
+
+while(n1 <= 9)
+{
+n2 = 0;
+while(n2 <= 9)
+{
+
+n3 = 0;
+while(n3 <= 9)
+{
+{
+if(n1 != n2 && nl < n2 && n2 != n3 && n2 < n3)
+{
+putchar(n1 + 48);
+putchar(n2 + 48);
+putchar(n3 + 48);
+
+if(n1 + n2 + n3 != 24
+{
+putchar(',');
+putchar(' ');
+}
+}
+++n3;
+}
+++n2;
+}
+++n1;
+}
+putchar('\n');
+return (0);
+}

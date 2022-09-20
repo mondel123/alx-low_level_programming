b245801b189cee795e6045a47339fd7abcc78c0f
mondel123 <adamsmonday1@gmail.com>
2022-09-19 17:03:34 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sign of a number
+ * @c: single letter input
+ * Return: Always success
+ */
+int print_sign(int c)
+{
+
+if (c > 0)
+
+{
+_putchar('+');
+return(1);
+}
+
+else if (n == 0)
+{
+
+_putchar('0');
+return (0);
+}
+
+else
+{
+
+putchar('_');
+return (-1);
+}
+}

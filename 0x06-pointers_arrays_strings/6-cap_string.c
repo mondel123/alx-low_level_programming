01e2eb196b8391850adb266d77057b15ce2b125f
mondel123 <adamsmonday1@gmail.com>
2022-09-22 05:47:55 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -9,14 +8,17 @@
 char *cap_string(char *str)
 {
 int index = 0;
+
 while (str[++index])
 {
 while (!(str[index] >= 'a' && str[index] <= 'z')
 index++;
+
 if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
+str[index - 1] == ';' ||
 str[index - 1] == '.' ||
 str[index - 1] == '!' ||
 str[index - 1] == '?' ||

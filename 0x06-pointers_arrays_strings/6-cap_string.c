ddcb44398ded330f0a94c0d6a7c3d6a312dffd14
mondel123 <adamsmonday1@gmail.com>
2022-09-22 06:31:47 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,10 +11,9 @@ int index = 0;
 
 while (str[++index])
 {
-while (!(str[index] >= 'a' && str[index] <= 'z')
-{
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
-}
+
 if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||

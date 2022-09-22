ae9d1ca070827e5987dc03d7e4a11d500201588a
mondel123 <adamsmonday1@gmail.com>
2022-09-22 01:31:44 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ int index = 0, dest_len = 0;
 while (dest[index++])
 dest_len++;
 
-for (index = o; src[index] && index < n; index++)
+for (index = 0; src[index] && index < n; index++)
 dest[dest_len++] = src[index];
 
 return (dest);

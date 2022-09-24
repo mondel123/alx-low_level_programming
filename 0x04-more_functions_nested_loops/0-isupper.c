46d0306c12cc15311aca9fa5a8081e3d0aefd86e
mondel123 <adamsmonday1@gmail.com>
2022-09-24 06:04:01 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 
 /**
- * _isupper - function to check if character is uppercase
+ * _isupper - function to check if 
+ * character is uppercase
+ *
  * @c: checks input of function
  *
- * Return: return 1 if 'c' is uppercase
+ * Return: return l if 'c' is uppercase
  * otherwise always 0 (success)
  */
 
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)
-return (1);
-
+return (l);
 return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 int _isupper(int c);
@@ -10,6 +11,8 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_squre(int size);
+void print_square(int size);
 void print_triangle(int size);
-void print_numbers(int n);
+void print_number(int n);
+
+#endif

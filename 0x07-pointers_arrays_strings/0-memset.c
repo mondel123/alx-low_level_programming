8dc229928c28b59a996f974fd9630338c0c5cd1f
mondel123 <adamsmonday1@gmail.com>
2022-09-26 16:23:37 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
  * Return: Always 0
  */
 
-char *_memset(char *s, char b, unsigned in n)
+char *_memset(char *s, char b, unsigned int n)
 {
 int i = 0;
 
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to);
 void print_diadsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif

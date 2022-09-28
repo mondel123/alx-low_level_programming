82f072bd0a368c337de4788efda6d61fb499a19c
mondel123 <adamsmonday1@gmail.com>
2022-09-28 04:29:37 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+* _strlen_recursion - size
+* @s: pointer to string params
+* Return: recursion
+*/
+
+int _strlen_recursion(char *s)
+{
+if (!*s)
+{
+return (0);
+}
+return (1 + _strlen_recursion(++s));
+}
+
+/**
 * p1 - palindrome
 * @s: pointer to string
 * @l: position

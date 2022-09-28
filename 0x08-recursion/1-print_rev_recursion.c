e0b0fd0af1e3bee95c673a34ac958ff7b5029860
mondel123 <adamsmonday1@gmail.com>
2022-09-28 02:52:01 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_rev_recursion - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: pointer block of memory to fill
  * Return: void
  */

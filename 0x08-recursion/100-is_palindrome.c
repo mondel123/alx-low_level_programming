87d9db76454ef1ae83b80a24d2b86dc8ba3809c7
mondel123 <adamsmonday1@gmail.com>
2022-09-28 04:20:06 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* p1 - palindrome
+* @s: pointer to string
+* @l: position
+* Return: boolean
+*/
+
+int p1(char *s, int l)
+{
+if (l < 1)
+{
+return (1);
+}
+
+if (*s == *(s + 1))
+{
+return (p1(s + 1, l - 2));
+}
+return (0);
+}
+
+/**
+* is_palindrome - palindrome
+* @s: pointer to string
+* Return: recursion
+*/
+
+int is_palindrome(char *s)
+{
+int len = _strlen_recursion(s);
+
+return (p1(s, len - 1));
+}

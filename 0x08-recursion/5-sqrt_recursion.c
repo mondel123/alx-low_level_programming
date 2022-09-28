a91f16e8ec6f1b1daa98b6f31c6ce53b3205136c
mondel123 <adamsmonday1@gmail.com>
2022-09-28 03:49:21 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,10 +12,10 @@ int _evaluate(int i, int n)
 if (n == 0 || n == 1)
 return (n);
 
-else if (i * 1 < n)
+else if (i * i < n)
 return (_evaluate(i + 1, n));
 
-else if (i * 1 == n) /*Base condtion*/
+else if (i * i == n) /*Base condtion*/
 return (i);
 
 return (-1);

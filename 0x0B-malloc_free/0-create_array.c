23c35b927ea5f6ac8e17adb992ad5234e5a0b18b
mondel123 <adamsmonday1@gmail.com>
2022-10-04 01:35:22 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,14 +15,18 @@ char *array;
 unsigned int index;
 
 if (size == 0)
+{
 return (NULL);
+}
 array = malloc(sizeof(char) * size);
 
 if (array == NULL)
 return (NULL);
 
 for (index = 0; index < size; index++)
+{
 array[index] = c;
+}
 
 return (array);
 }

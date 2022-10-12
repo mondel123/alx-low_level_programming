9a73de72362c718273c4a9fea86a97b2de8c8956
mondel123 <adamsmonday1@gmail.com>
2022-10-12 04:21:15 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,6 +23,13 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	res = (get_op_func(argv[2]))(a, b);

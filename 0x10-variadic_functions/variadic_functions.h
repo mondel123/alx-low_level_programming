d5a04fcd461810bbc9520feb77f0b6712c6b8840
mondel123 <adamsmonday1@gmail.com>
2022-10-13 07:37:22 -0700

    variadic_functions.h correction

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int sun_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);

c5e18046cf3eb79b26f351078fea69e3739584c1
mondel123 <adamsmonday1@gmail.com>
2022-10-20 15:14:04 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - function executed before main
+ * Return: no return
+ */
+
+void _attribute_ ((constructor)) first()
+{
+printf("you're best! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
+}

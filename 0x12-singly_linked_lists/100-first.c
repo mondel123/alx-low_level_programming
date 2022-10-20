59a4cf7d4cc2e6a303db9cb62d123632a77038a0
mondel123 <adamsmonday1@gmail.com>
2022-10-20 15:36:45 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return
  */
 
-void _attribute_ ((constructor)) first()
+void _attribute_((constructor)) first()
 {
 	printf("you're best! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");

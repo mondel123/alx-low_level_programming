21c723e561f35774d5f323b0d05758dece3a98dc
mondel123 <adamsmonday1@gmail.com>
2022-10-20 15:20:49 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 
 void _attribute_ ((constructor)) first()
 {
-printf("you're best! and yet, you must allow");
-printf(",\nI bore my house upon my back!\n");
+	printf("you're best! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
